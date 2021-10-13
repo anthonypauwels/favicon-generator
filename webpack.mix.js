1afb0d45ee97c3458acddec127c163d3d1102b9c
Anthony Pauwels <anthony@cherrypulp.com>
2021-10-13 22:46:03 +0200

    moving content from app directory to build directory

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,23 +4,23 @@ mix.webpackConfig({
     target: 'electron-renderer',
 });
 
-mix.js('app/assets/scripts/main.js', 'scripts');
+mix.js('build/assets/scripts/main.js', 'scripts');
 
-mix.sass('app/assets/styles/main.scss', 'styles')
+mix.sass('build/assets/styles/main.scss', 'styles')
     .options({
         processCssUrls: false
     });
 
-mix.sass('app/assets/styles/regular-theme.scss', 'styles')
+mix.sass('build/assets/styles/regular-theme.scss', 'styles')
     .options({
         processCssUrls: false
     });
 
-mix.sass('app/assets/styles/cherry-pulp.scss', 'styles')
+mix.sass('build/assets/styles/cherry-pulp.scss', 'styles')
     .options({
         processCssUrls: false
     });
 
-mix.setPublicPath('app/render');
+mix.setPublicPath('build/render');
 
 mix.disableNotifications();
